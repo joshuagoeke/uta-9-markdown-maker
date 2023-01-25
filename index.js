8b25099fdddae9c5ce7b59300153f6eba2c49a22
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-24 23:53:52 -0600

    Thanks karenpeazoni for helping me connect a few dots with the initialization. Got working inquirer

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const renderLicenseBadge = require('./utils/generateMarkdown.js')
+
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name?'
+            name: 'github',
+            message: 'What is your GitHub Username?'
+        },
+        {
+            type: 'input',
+            name: 'notEmail',
+            message: 'What is your fake email that will be prominantly displayed where any bot or creep can find it in the plain view of the entire internet? (Please do not use your real email.)'
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your Project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please tell us about your project, especially since it is this program you are using. What are we doing right now?'
+        },
+        {
+            type: 'checkbox',
+            name: 'instructions',
+            message: 'Please choose all that apply:',
+            choices: [
+                'Install Node.js', 'run in VS integrated terminal: npm init -y', 'run in integrated terminal: npm i inquirer@8.2.4', 'run in integrated terminal: node index.js'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'use',
+            message: 'How will people use this amazing product?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How will people help make this thing more awesome?'
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'How will people test this product?:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Pick one of these licenses.',
+            choices: [
+                'MIT', 'PPDL', 'The Unlicense',
+            ]
         },
     ])
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 const init = () => {
-    questions()
-        .then((answers) => writeToFile(README.md, questions(answers)))
-        .then(() => console.log('Check out that slick new README.md over there! ;) '))
-        .catch((err) => console.error(err));
+    inquirer.prompt(questions).then((response)=>{
+        console.log(response)
+        writeToFile("README.md", generateMarkdown(response))
+    });    
 };
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,156 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // switch using code snippets from https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+  switch(selection) {
+    case 'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+      case 'PDDL':
+      return `[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)`;
+      break;
+      case 'The Unlicense':
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+      break;
+      default: 
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(selection) {
+    case 'MIT':
+      return `https://opensource.org/licenses/MIT`;
+      break;
+      case 'PDDL':
+      return `https://opendatacommons.org/licenses/pddl/`;
+      break;
+      case 'The Unlicense':
+      return `http://unlicense.org/`;
+      break;
+      default: 
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch(selection) {
+    case 'MIT':
+      return `
+      Copyright 2023 
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+      break;
+      case 'PDDL':
+      return `Open Data Commons Public Domain Dedication and License (PDDL) Summary
+      This is a human-readable summary of the Public Domain Dedication and License 1.0. Please see the disclaimer below.
+      
+      You are free:
+      
+      To share: To copy, distribute and use the database.
+      To create: To produce works from the database.
+      To adapt: To modify, transform and build upon the database.
+      As long as you:
+      
+      Blank: This section is intentionally left blank. The PDDL imposes no restrictions on your use of the PDDL licensed database.
+      Disclaimer
+      
+      This is not a license. It is simply a handy reference for understanding the PDDL 1.0 — it is a human-readable expression of some of its key terms. This document has no legal value, and its contents do not appear in the actual license. Read the full PDDL 1.0 license text for the exact terms that apply.`;
+      break;
+      case 'The Unlicense':
+      return `This is free and unencumbered software released into the public domain.
+
+      Anyone is free to copy, modify, publish, use, compile, sell, or
+      distribute this software, either in source code form or as a compiled
+      binary, for any purpose, commercial or non-commercial, and by any
+      means.
+      
+      In jurisdictions that recognize copyright laws, the author or authors
+      of this software dedicate any and all copyright interest in the
+      software to the public domain. We make this dedication for the benefit
+      of the public at large and to the detriment of our heirs and
+      successors. We intend this dedication to be an overt act of
+      relinquishment in perpetuity of all present and future rights to this
+      software under copyright law.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+      IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+      OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+      OTHER DEALINGS IN THE SOFTWARE.
+      
+      For more information, please refer to <http://unlicense.org/>`;
+      break;
+      default: 
+      return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+  
+  ${data.description}
 
+  This project is an assignment for the University of Texas - Austin coding bootcamp, and here is a description of the acceptance criteria:
+
+  ## Acceptance Criteria
+
+  GIVEN a command-line application that accepts user input
+  WHEN I am prompted for information about my application repository
+  THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+  WHEN I enter my project title
+  THEN this is displayed as the title of the README
+  WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+  THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+  WHEN I choose a license for my application from a list of options
+  THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+  WHEN I enter my GitHub username
+  THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+  WHEN I enter my email address
+  THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+  WHEN I click on the links in the Table of Contents
+  THEN I am taken to the corresponding section of the README
+  
+  ## Table of Conents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributions](#contributions)
+  - [Testing](#testing)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.instructions}
+  ## Usage
+  ${data.use}
+  ## Contributions 
+  ${data.contribution}
+  ## Testing
+  ${data.testing}
+  ## License
+  ${data.license}
+  ## Questions
+  If you have further questions, find me on GitHub at https://github.com/${github}
+  Please don't email me at ${notEmail}, because I'm not an idiot and that isn't a real email address.
+
+  ##Credits
+  Shout out to 
 `;
 }
+
+//need to run functions on license, I think...
 
 module.exports = generateMarkdown;
